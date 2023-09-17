f16223ac191b38fa984bdb595e3790a062577511
Taka <103984069+Taka0007@users.noreply.github.com>
2023-09-17 18:45:21 +0900

    Update AOJ-NTL-1-D.test.py

--- verify-check/AOJ-NTL-1-D.test.py
+++ verify-check/AOJ-NTL-1-D.test.py
@@ -1,6 +1,6 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_D
 
-from num-theory.Eulers_totient_function import Eulers_totient_function
+from num_theory.Eulers_totient_function import Eulers_totient_function
 
 N = int(input())
 print(Eulers_totient_function(N))
