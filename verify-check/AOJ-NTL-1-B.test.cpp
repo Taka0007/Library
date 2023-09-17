ea26b57da50d808d21f195ff6003aa1f04f4f1fc
Taka <103984069+Taka0007@users.noreply.github.com>
2023-09-17 18:49:22 +0900

    Update AOJ-NTL-1-B.test.cpp

--- verify-check/AOJ-NTL-1-B.test.cpp
+++ verify-check/AOJ-NTL-1-B.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B"
 #include <iostream>
-#include "num-theory/pow.cpp"
+#include "num_theory/pow.cpp"
 using namespace std;
 int main() {
     long long m, n;
