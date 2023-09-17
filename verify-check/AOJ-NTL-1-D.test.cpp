a5dc03015b25b54cc58c26b0313ce36f87ab46cf
Taka <103984069+Taka0007@users.noreply.github.com>
2023-09-17 18:53:47 +0900

    Create AOJ-NTL-1-D.test.cpp

---
+++ verify-check/AOJ-NTL-1-D.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/NTL_1_D"
+#include <iostream>
+#include "Eulers_totient_function.cpp"
+using namespace std;
+int main() {
+    int main() {
+    int N;
+    cin >> N;
+    cout << EulersTotientFunction(N) << endl;
+    return 0;
+}
