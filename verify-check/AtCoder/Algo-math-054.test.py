5c32c18fed2cf94109ec5c8dbd3ae5efdbe4c07e
Taka <103984069+Taka0007@users.noreply.github.com>
2023-09-21 14:28:58 +0900

    Create Algo-math-054.test.py

---
+++ verify-check/AtCoder/Algo-math-054.test.py
@@ -0,0 +1,30 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_at
+
+
+import numpy as np
+
+def dot(A1, A2, N, mod):
+    # N: 行列のサイズ
+    A = np.zeros((N, N), dtype=int)
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                A[i][j] = (A[i][j] + (A1[i][k] * A2[k][j]) % mod) % mod
+    return A
+
+def pow_mat(A, K, N, mod):
+    # A: 累乗する行列, k: 累乗数, n: 行列Aのサイズ
+    P = np.eye(N, dtype=int)
+    while K:
+        if K & 1:
+            P = dot(P, A, N, mod)
+        A = dot(A, A, N, mod)
+        K >>= 1
+    return P
+
+N = int(input())
+mat = np.array([[1, 1], [1, 0]], dtype=int)
+mod = 10**9
+ans_matrix = pow_mat(mat, N-1, 2, mod)
+ans = (ans_matrix[1][0] + ans_matrix[1][1]) % mod
+print(ans)
